b9dd64082e756840f8c9f0b05316aec0722139f6
khokar <khokarritik1999@gmail.com>
2023-09-14 16:14:50 +0530

    master|navbar component created

---
+++ public/module1.mjs
@@ -0,0 +1,8 @@
+let a = 3;
+let b= 2;
+let c= 4;
+let d= 5;
+let r= 6;
+export default a;
+export {b}
+export{c,d,r}
---
+++ public/module2.mjs
@@ -0,0 +1,6 @@
+import val, {b,c,d,r} from "./module1.mjs"
+console.log(val);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(r);
